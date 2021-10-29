ace08f97776c5768b867ccfc5f26ff7a41d69155
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2021-10-29 23:01:47 +0300

    add cache

--- src/store/actions/fetch-news.js
+++ src/store/actions/fetch-news.js
@@ -1,15 +1,30 @@
 import axios from "axios";
 import { API_KEY, BASE_URL } from "../constants/newsActionTypes";
 import { loadedNews } from "./newsActions";
+import localforage from "localforage";
+
+export const fileCache = localforage.createInstance({
+  name: "filecache",
+});
 
 export const fetchNews = (category) => {
   return async (dispatch) => {
+    const cachedResult = await fileCache.getItem(category);
+
+    if (cachedResult) {
+      // console.log(cachedResult);
+      dispatch(loadedNews(cachedResult.articles));
+      return cachedResult.articles;
+    }
     // const response = await axios(`${BASE_URL}&q=${category}&apiKey=${API_KEY}`);
     const response = await axios(`${BASE_URL}&q=${category}&apiKey=${API_KEY}`);
     console.log(response);
     const data = await response.data;
     console.log(data.articles);
     dispatch(loadedNews(data.articles));
+
+    await fileCache.setItem(category, data);
+
     return data;
   };
 };
